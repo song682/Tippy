27cfb51c091bc4fd6d6a2e400bcc56b9cf7e1b63
dfdvdsf <81430928+song682@users.noreply.github.com>
2025-07-15 15:28:49 +0800

    Add files via upload

---
+++ src/main/tips/mixin/MixinGuiDisconnected.java
@@ -0,0 +1,17 @@
+package decok.dfcdvadstf.tips.mixin;
+
+import decok.dfcdvadstf.tips.TipsRenderer;
+import net.minecraft.client.gui.GuiDisconnected;
+import net.minecraft.client.gui.GuiScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GuiDisconnected.class)
+public class MixinGuiDisconnected extends GuiScreen {
+    @Inject(method = "drawScreen", at = @At("TAIL"))
+    public void onDrawScreen(int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
+        TipsRenderer.renderTip(this);
+    }
+}
