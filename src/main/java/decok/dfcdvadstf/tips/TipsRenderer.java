02e4936961f0f4db94f6a16dff797838f0b14e0a
dfdvdsf <81430928+song682@users.noreply.github.com>
2025-07-19 00:32:49 +0800

    Update TipsRenderer.java

--- src/main/java/decok/dfcdvadstf/tips/TipsRenderer.java
+++ src/main/java/decok/dfcdvadstf/tips/TipsRenderer.java
@@ -3,11 +3,13 @@ package decok.dfcdvadstf.tips;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.StatCollector;
+import net.minecraft.client.resources.I18n;
 import org.lwjgl.opengl.GL11;
+
 
 public class TipsRenderer {
     public static void renderTip(GuiScreen screen) {
-        
+
         // 获取 Minecraft 实例 (允许我使用 FontRenderer)
         // Get Minecraft instance to allow me use FontRenderer
         Minecraft mc = Minecraft.getMinecraft();
@@ -23,8 +25,12 @@ public class TipsRenderer {
             yPos = screen.height + yPos;
         }
 
+
+
         // 获取本地化文本
         // Get localized text
+        // String title = I18n.format("tippy.title");
+        // String tip = I18n.format(Tippy.getCurrentTipKey());
         String title = StatCollector.translateToLocal("tippy.title");
         String tip = StatCollector.translateToLocal(Tippy.getCurrentTipKey());
 
@@ -50,6 +56,7 @@ public class TipsRenderer {
                 yPos + 10,
                 Tippy.contentColor
         );
+
         // ??? I don't understand.
         // ？？？我不理解？！
          GL11.glDisable(GL11.GL_BLEND);
