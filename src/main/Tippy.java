c6d8ded6c9365cb08410d44f81c825a5081c29c3
dfdvdsf <81430928+song682@users.noreply.github.com>
2025-08-25 22:50:10 +0800

    Add Tags.java

--- src/main/Tippy.java
+++ src/main/Tippy.java
@@ -21,10 +21,8 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-@Mod(modid = Tippy.MODID, version = Tippy.VERSION, name = "Tippy")
-public class Tippy {
-    public static final String MODID = "tippy";
-    public static final String VERSION = "1.6.0"; // 版本号更新 // Update the version number
+@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME)
+public class Tippy { 
     public static Logger logger;
 
     // Config default setting
@@ -153,4 +151,5 @@ public class Tippy {
             lastSwitchTime = currentTime;
         }
     }
+
 }
